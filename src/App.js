be85d2f811a6fbd94f16dc482bb5248c5a691a92
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-01-09 03:50:07 +0600

    Second change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
     <div className="App">
-      <h2>Second branch</h2>
+      <h2>Second branch ?</h2>
+      <p>Why are you doing this?</p>
       <div>
       </div>
     </div>
